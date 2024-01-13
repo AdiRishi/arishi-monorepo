a0870477099becc26d5c96de961a989a11c233af
Adishwar Rishi <adiswa123@gmail.com>
2024-01-13 18:19:07 +1100

    Configure eslint and typescript and remove base apps

---
+++ packages/eslint-config/base.js
@@ -0,0 +1,38 @@
+const { resolve } = require('node:path');
+
+const project = resolve(process.cwd(), 'tsconfig.json');
+
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project,
+      },
+    },
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  ignorePatterns: ['.eslintrc.json'],
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^_',
+      },
+    ],
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+  },
+};
