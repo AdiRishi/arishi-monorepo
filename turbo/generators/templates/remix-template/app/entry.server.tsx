076017f3a3d8ad92092272bd00e0235939e48ae3
Adishwar Rishi <adiswa123@gmail.com>
2024-01-14 14:49:46 +1100

    Modify remix template to use repo eslint config package

--- turbo/generators/templates/remix-template/app/entry.server.tsx
+++ turbo/generators/templates/remix-template/app/entry.server.tsx
@@ -27,7 +27,7 @@ export default async function handleRequest(
     },
   });
 
-  if (isbot(request.headers.get('user-agent') || '')) {
+  if (isbot(request.headers.get('user-agent') ?? '')) {
     await body.allReady;
   }
 
