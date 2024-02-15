31994a526c7fa0969fef8c35517f641d90f7d80d
Adishwar Rishi <adiswa123@gmail.com>
2024-02-15 22:11:30 +1100

    Check

--- apps/arishi-website-webapp/app/routes/_index/route.tsx
+++ apps/arishi-website-webapp/app/routes/_index/route.tsx
@@ -44,6 +44,7 @@ function IntroSection() {
   return (
     <div className="flex items-center justify-between">
       <div className="flex-1 space-y-1.5">
+        Hello does this even work?
         <h1 className="text-2xl font-bold">{RESUME_DATA.name}</h1>
         <p className="max-w-md text-pretty font-mono text-sm text-muted-foreground">{RESUME_DATA.about}</p>
         <p className="max-w-md items-center text-pretty font-mono text-xs text-muted-foreground">
