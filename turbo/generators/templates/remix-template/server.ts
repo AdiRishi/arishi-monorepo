1d41631e188ddc3f13435f63b39c22a66353c67f
Adishwar Rishi <adiswa123@gmail.com>
2024-03-01 11:02:33 +1100

    Update remix templates and rebuild lockfile

--- turbo/generators/templates/remix-template/server.ts
+++ turbo/generators/templates/remix-template/server.ts
@@ -1,4 +1,5 @@
 import { logDevReady } from '@remix-run/cloudflare';
+import type { AppLoadContext } from '@remix-run/cloudflare';
 import { createPagesFunctionHandler } from '@remix-run/cloudflare-pages';
 import * as build from '@remix-run/dev/server-build';
 
@@ -6,8 +7,7 @@ if (process.env.NODE_ENV === 'development') {
   logDevReady(build);
 }
 
-export const onRequest = createPagesFunctionHandler({
+export const onRequest = createPagesFunctionHandler<AppLoadContext['env']>({
   build,
-  getLoadContext: (context) => ({ env: context.env }),
-  mode: build.mode,
+  getLoadContext: ({ context }) => ({ env: context.cloudflare.env }),
 });
