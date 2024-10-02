791d8de3c33581bfcfde5143d3901f31dc2a441c
Adishwar Rishi <adiswa123@gmail.com>
2024-10-02 12:53:39 +1000

    Implement domain redirect worker

--- cf-workers/domain-redirect/src/index.ts
+++ cf-workers/domain-redirect/src/index.ts
@@ -1,8 +1,24 @@
 import { Env, LocalExportedHandler } from './env';
 
+export const DOMAIN_MAP: Record<string, string> = {
+  'adishwarrishi.com': 'adishwar-rishi.com',
+  'www.adishwarrishi.com': 'www.adishwar-rishi.com',
+  'turborepo-remote-cache.dev': 'cloudflare.turborepo-remote-cache.dev',
+  'www.turborepo-remote-cache.dev': 'cloudflare.turborepo-remote-cache.dev',
+};
+
 const handler: LocalExportedHandler<Env> = {
-  async fetch(request, env, ctx): Promise<Response> {
-    return new Response('Hello World!');
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async fetch(request, _env, _ctx): Promise<Response> {
+    const url = new URL(request.url);
+    const mappedDomain: string | undefined = DOMAIN_MAP[url.hostname];
+    if (mappedDomain) {
+      const redirectUrl = `${url.protocol}//${mappedDomain}${url.pathname}${url.search}`;
+      return Response.redirect(redirectUrl, 301);
+    }
+
+    console.log('In error stage');
+    return Response.json({ error: `No domain mapping found for URL ${url.toString()}` }, { status: 404 });
   },
 };
 
--- cf-workers/domain-redirect/test/index.spec.ts
+++ cf-workers/domain-redirect/test/index.spec.ts
@@ -1,25 +1,52 @@
 // test/index.spec.ts
 import { env, createExecutionContext, waitOnExecutionContext, SELF } from 'cloudflare:test';
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeEach } from 'vitest';
 import worker from '../src/index';
 
 // For now, you'll need to do something like this to get a correctly-typed
 // `Request` to pass to `worker.fetch()`.
 const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
 
-describe('Hello World worker', () => {
-  it('responds with Hello World! (unit style)', async () => {
-    const request = new IncomingRequest('http://example.com');
-    // Create an empty context to pass to `worker.fetch()`.
-    const ctx = createExecutionContext();
-    const response = await worker.fetch(request, env, ctx);
-    // Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
-    await waitOnExecutionContext(ctx);
-    expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+describe('Domain Redirect worker', () => {
+  describe('Unit Tests', () => {
+    let ctx: ExecutionContext;
+
+    beforeEach(() => {
+      ctx = createExecutionContext();
+    });
+
+    it('redirects adishwarrishi.com to adishwar-rishi.com', async () => {
+      const request = new IncomingRequest('http://adishwarrishi.com');
+      const response = await worker.fetch(request, env, ctx);
+      await waitOnExecutionContext(ctx);
+      expect(response.status).toBe(301);
+      expect(response.headers.get('Location')).toBe('http://adishwar-rishi.com/');
+    });
+
+    it('returns 404 for unmapped domain', async () => {
+      const request = new IncomingRequest('http://unmapped-domain.com');
+      const response = await worker.fetch(request, env, ctx);
+      await waitOnExecutionContext(ctx);
+      expect(response.status).toBe(404);
+      const jsonResponse = await response.json<{ error: string }>();
+      expect(jsonResponse.error).toBe('No domain mapping found for URL http://unmapped-domain.com/');
+    });
   });
 
-  it('responds with Hello World! (integration style)', async () => {
-    const response = await SELF.fetch('https://example.com');
-    expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+  describe('Integration Tests', () => {
+    it.only('redirects adishwarrishi.com to adishwar-rishi.com', async () => {
+      const response = await SELF.fetch('http://adishwarrishi.com', {
+        redirect: 'manual',
+      });
+      expect(response.status).toBe(301);
+      expect(response.headers.get('Location')).toBe('http://adishwar-rishi.com/');
+    });
+
+    it('returns 404 for unmapped domain', async () => {
+      const response = await SELF.fetch('https://unmapped-domain.com');
+      expect(response.status).toBe(404);
+      const jsonResponse = await response.json<{ error: string }>();
+      expect(jsonResponse.error).toBe('No domain mapping found for URL https://unmapped-domain.com/');
+    });
   });
 });
