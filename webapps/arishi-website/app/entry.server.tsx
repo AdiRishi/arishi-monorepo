a47c86881ce4ba0a0e637afcecdf179d8fab37e4
Adishwar Rishi <adiswa123@gmail.com>
2024-11-08 17:24:25 +1100

    chore: Update all packages

--- webapps/arishi-website/app/entry.server.tsx
+++ webapps/arishi-website/app/entry.server.tsx
@@ -22,7 +22,7 @@ export default async function handleRequest(
     },
   });
 
-  if (isbot(request.headers.get('user-agent') || '')) {
+  if (isbot(request.headers.get('user-agent') ?? '')) {
     await body.allReady;
   }
 
